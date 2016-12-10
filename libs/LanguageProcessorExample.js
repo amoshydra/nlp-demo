d59d3810a077628f4c35c78a9a031b4b9170fe89
Amos Wong <amoswwj@gmail.com>
2016-12-10 22:15:59 +0800

    Fix indentation

--- libs/LanguageProcessorExample.js
+++ libs/LanguageProcessorExample.js
@@ -3,9 +3,9 @@ var processor = require('./languageProcessor');
 var results = processor('hello world');
 
 results.forEach(function(result) {
-  console.log(`Input string: \n >> ${result.str} \n`);
-  result.terms.forEach(function(term) {
-    console.log(term);
-  });
-  console.log('\n');
+	console.log(`Input string: \n >> ${result.str} \n`);
+	result.terms.forEach(function(term) {
+		console.log(term);
+	});
+	console.log('\n');
 });
